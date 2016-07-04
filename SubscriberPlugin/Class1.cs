e7e8df3c544c45226c482f8c1810f517afbd14f9
dxiao3 <Dingming.Xiao@morningstar.com>
2016-07-04 15:32:44 +0800

    Start

---
+++ SubscriberPlugin/Class1.cs
@@ -0,0 +1,48 @@
+﻿using MainApp;
+using MainApp.plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriberPlugin
+{
+    public class SubPlugin : ISubscribe
+    {
+        string name = "SubPlugin";
+        public string PluginName
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public void LoadConfig()
+        {
+           if(PluginManager.eventPlugins != null && PluginManager.eventPlugins.Count>0)
+            {
+                for(int i = 0;i<PluginManager.eventPlugins.Count;i++)
+                {
+                    PluginManager.eventPlugins[i].TestEvent += SubPlugin_TestEvent;
+                }
+            }
+        }
+
+        private void SubPlugin_TestEvent(object sender, EventArgs e)
+        {
+            if(sender!=null)
+                Console.WriteLine(sender.ToString());
+        }
+
+        public void Test()
+        {
+            Console.WriteLine("test func");
+        }
+    }
+}
---
+++ SubscriberPlugin/UnitTest.cs
@@ -0,0 +1,28 @@
+﻿
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SubscriberPlugin
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        public SubPlugin Plugin { get; set; }
+        [SetUp]
+        public void SetUp()
+        {
+            Plugin = new SubPlugin();
+        }
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
